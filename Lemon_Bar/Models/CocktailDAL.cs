b43fba2d4342b3eea93bdae0015f67b31f7c2755
gesawyer <74635177+gesawyer@users.noreply.github.com>
2021-03-12 16:58:50 -0500

    added API

---
+++ Lemon_Bar/Models/CocktailDAL.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Lemon_Bar.Models
+{
+    public class CocktailDAL
+    {
+
+        public string GetData(string searchName)
+        {
+            //WILL MODIFY THE SEARCH
+            string url = $"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={searchName}";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = null;
+
+            response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string json = rd.ReadToEnd();
+          
+            return json;
+        }
+
+        public Rootobject GetDataString(string searchName)
+        {
+            string json = GetData(searchName);
+            Rootobject r = JsonConvert.DeserializeObject<Rootobject>(json);
+            return r;
+
+            
+        }
+        
+    }
+}
---
+++ Lemon_Bar/Secret.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lemon_Bar
+{
+    public class Secret
+    {
+        public static string connection { get; } = "Server = tcp:wendiserver.database.windows.net,1433;Initial Catalog = Lemon_Bar; Persist Security Info=False;User ID = wendiserveradmin; Password=Admin123; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30";
+
+
+        //wendiserveradmin
+        //Admin123
+    }
+}
