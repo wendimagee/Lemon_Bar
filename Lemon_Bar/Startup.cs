e1912633ec1ac0f9a5cf118ae030aa65fb495c3b
Wendi Magee <wendimagee@gmail.com>
2021-03-15 09:29:56 -0400

    Fixed the startup to incorporate API correctly. IT's working!!

--- Lemon_Bar/Startup.cs
+++ Lemon_Bar/Startup.cs
@@ -8,8 +8,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,96 +19,57 @@ namespace Lemon_Bar
 {
     public class Startup
     {
-
-        string connection = Secret.connection;
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
+
         public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer("DefaultConnection"));
         }
+
+        //This method gets called by the runtime.Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
             app.UseRouting();
+
+            app.UseAuthentication();
             app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                                    name: "default",
-                                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
- 
-            
         }
-        //string connection = "Server=.\\SQLExpress;Database=;Trusted_Connection=True;";
-
-
-        //public Startup(IConfiguration configuration)
-        //{
-        //    Configuration = configuration;
-        //}
-
-        //public IConfiguration Configuration { get; }
-
-        //// This method gets called by the runtime. Use this method to add services to the container.
-        //public void ConfigureServices(IServiceCollection services)
-        //{
-        //    services.AddDbContext<ApplicationDbContext>(options =>
-        //        options.UseSqlServer(
-        //            Configuration.GetConnectionString("DefaultConnection")));
-        //    services.AddDatabaseDeveloperPageExceptionFilter();
-
-        //    services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-        //        .AddEntityFrameworkStores<ApplicationDbContext>();
-        //    services.AddControllersWithViews();
-        //    services.AddDbContext<ApplicationDbContext>(options =>
-        //        options.UseSqlServer(connection));
-        //}
-
-        ////This method gets called by the runtime.Use this method to configure the HTTP request pipeline.
-        //public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        //{
-        //    if (env.IsDevelopment())
-        //    {
-        //        app.UseDeveloperExceptionPage();
-        //        app.UseMigrationsEndPoint();
-        //    }
-        //    else
-        //    {
-        //        app.UseExceptionHandler("/Home/Error");
-        //        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        //        app.UseHsts();
-        //    }
-        //    app.UseHttpsRedirection();
-        //    app.UseStaticFiles();
-
-        //    app.UseRouting();
-
-        //    app.UseAuthentication();
-        //    app.UseAuthorization();
-
-        //    app.UseEndpoints(endpoints =>
-        //    {
-        //        endpoints.MapControllerRoute(
-        //            name: "default",
-        //            pattern: "{controller=Home}/{action=Index}/{id?}");
-        //        endpoints.MapRazorPages();
-        //    });
-        //}
     }
 }
