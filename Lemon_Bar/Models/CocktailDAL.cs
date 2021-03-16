101289750bd24fa23d0faeeaf56516e19e823e3a
Nate <nsdavis@mtu.edu>
2021-03-16 11:38:39 -0400

    Fixed Drink Details and added SearchId in DAL

--- Lemon_Bar/Models/CocktailDAL.cs
+++ Lemon_Bar/Models/CocktailDAL.cs
@@ -13,7 +13,7 @@ namespace Lemon_Bar.Models
 
         public string GetData(string searchName)
         {
-            //WILL MODIFY THE SEARCH
+            //Searches by Name
             string url = $"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={searchName}";
 
             HttpWebRequest request = WebRequest.CreateHttp(url);
@@ -33,6 +33,30 @@ namespace Lemon_Bar.Models
             return r;
 
             
+        }
+
+        public string GetIdData(int id)
+        {
+            //Searches by ID
+            string url = $"https://www.thecocktaildb.com/api/json/v2/1/lookup.php?i={id}";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = null;
+
+            response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string json = rd.ReadToEnd();
+
+            return json;
+        }
+
+        public Rootobject GetIdDataString(int id)
+        {
+            string json = GetIdData(id);
+            Rootobject r = JsonConvert.DeserializeObject<Rootobject>(json);
+            return r;
+
+
         }
 
         //this link is for searching by multiple ingredients https://www.thecocktaildb.com/api/json/v2/1/filter.php?i=vodka&&lemon&&lime&&cranberry
