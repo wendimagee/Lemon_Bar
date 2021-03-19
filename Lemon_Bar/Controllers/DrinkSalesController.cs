6ada7067158139e4a8ebd5c51dd9b16303917484
Wendi Magee <wendimagee@gmail.com>
2021-03-19 13:18:19 -0400

    Added functionality to handle measurements with "top with" or "fill with"

--- Lemon_Bar/Controllers/DrinkSalesController.cs
+++ Lemon_Bar/Controllers/DrinkSalesController.cs
@@ -198,6 +198,13 @@ namespace Lemon_Bar.Controllers
                         item.Quantity -= (double)(measurement);
                         _context.Items.Update(item);
                     }
+                    else if (drink.strMeasure1.ToLower().Contains("Top")|| drink.strMeasure1.ToLower().Contains("Fill"))
+                    {
+                        decimal? measurement = 2.0m;
+                        netCost += item.UnitCost * measurement;
+                        item.Quantity -= (double)(measurement);
+                        _context.Items.Update(item);
+                    }
                 }
                 else if (drink.strIngredient2.ToLower() == item.ItemName.ToLower())
                 {
@@ -233,6 +240,13 @@ namespace Lemon_Bar.Controllers
                     {
                         decimal? measurement = ConvertFromMl(drink.strMeasure2);
                         netCost += item.UnitCost * (measurement * 100);
+                        item.Quantity -= (double)(measurement);
+                        _context.Items.Update(item);
+                    }
+                    else if (drink.strMeasure2.ToLower().Contains("Top") || drink.strMeasure2.ToLower().Contains("Fill"))
+                    {
+                        decimal? measurement = 2.0m;
+                        netCost += item.UnitCost * measurement;
                         item.Quantity -= (double)(measurement);
                         _context.Items.Update(item);
                     }
@@ -274,6 +288,13 @@ namespace Lemon_Bar.Controllers
                         item.Quantity -= (double)(measurement);
                         _context.Items.Update(item);
                     }
+                    else if (drink.strMeasure3.ToLower().Contains("Top") || drink.strMeasure3.ToLower().Contains("Fill"))
+                    {
+                        decimal? measurement = 2.0m;
+                        netCost += item.UnitCost * measurement;
+                        item.Quantity -= (double)(measurement);
+                        _context.Items.Update(item);
+                    }
                 }
                 else if (drink.strIngredient4 != null && drink.strIngredient4.ToLower() == item.ItemName.ToLower())
                 {
@@ -309,6 +330,13 @@ namespace Lemon_Bar.Controllers
                     {
                         decimal? measurement = ConvertFromMl(drink.strMeasure4);
                         netCost += item.UnitCost * (measurement * 100);
+                        item.Quantity -= (double)(measurement);
+                        _context.Items.Update(item);
+                    }
+                    else if (drink.strMeasure4.ToLower().Contains("Top") || drink.strMeasure4.ToLower().Contains("Fill"))
+                    {
+                        decimal? measurement = 2.0m;
+                        netCost += item.UnitCost * measurement;
                         item.Quantity -= (double)(measurement);
                         _context.Items.Update(item);
                     }
@@ -350,6 +378,13 @@ namespace Lemon_Bar.Controllers
                         item.Quantity -= (double)(measurement);
                         _context.Items.Update(item);
                     }
+                    else if (drink.strMeasure5.ToLower().Contains("Top") || drink.strMeasure5.ToLower().Contains("Fill"))
+                    {
+                        decimal? measurement = 2.0m;
+                        netCost += item.UnitCost * measurement;
+                        item.Quantity -= (double)(measurement);
+                        _context.Items.Update(item);
+                    }
                 }
                 else if (drink.strIngredient6 != null && drink.strIngredient6.ToLower() == item.ItemName.ToLower())
                 {
@@ -385,6 +420,13 @@ namespace Lemon_Bar.Controllers
                     {
                         decimal? measurement = ConvertFromMl(drink.strMeasure6);
                         netCost += item.UnitCost * (measurement * 100);
+                        item.Quantity -= (double)(measurement);
+                        _context.Items.Update(item);
+                    }
+                    else if (drink.strMeasure6.ToLower().Contains("Top") || drink.strMeasure6.ToLower().Contains("Fill"))
+                    {
+                        decimal? measurement = 2.0m;
+                        netCost += item.UnitCost * measurement;
                         item.Quantity -= (double)(measurement);
                         _context.Items.Update(item);
                     }
