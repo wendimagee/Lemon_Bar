30645f7e04241839d256f44d58024dd206a4bea6
Jeffrey <JWohlfield@hotmail.com>
2021-03-22 11:41:29 -0400

    Added IngredType table to database

---
+++ Lemon_Bar/Models/IngredientType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Lemon_Bar.Models
+{
+    public partial class IngredientType
+    {
+        public int Id { get; set; }
+        public string ApiingId { get; set; }
+        public string ApistrIngredient { get; set; }
+        public string ApistrType { get; set; }
+        public string ApistrAlcohol { get; set; }
+        public string ApistrAbv { get; set; }
+        public string IngCategory { get; set; }
+    }
+}
